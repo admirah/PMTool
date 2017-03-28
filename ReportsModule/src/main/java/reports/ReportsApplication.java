78a570162eb7efcaafbf55dc4c12e8cf8c6e6b97
Admira <ahusic5@etf.unsa.ba>
2017-03-28 16:05:14 +0200

    spring security

--- ReportsModule/src/main/java/reports/ReportsApplication.java
+++ ReportsModule/src/main/java/reports/ReportsApplication.java
@@ -1,12 +1,31 @@
 package reports;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class ReportsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReportsApplication.class, args);
+	}
+}
+
+@RefreshScope
+@RestController
+class MessageRestController {
+
+
+
+	@Value("${message:Hello default}")
+	private String message;
+
+	@RequestMapping("/message")
+	String getMessage() {
+		return this.message;
 	}
 }
