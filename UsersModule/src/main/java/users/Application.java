0a4aae675d0805186b6f9b4fbf95ee84cd79124c
AmarBasic <amarbasic4@gmail.com>
2017-03-28 22:13:54 +0200

    test

--- UsersModule/src/main/java/users/Application.java
+++ UsersModule/src/main/java/users/Application.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -30,6 +32,7 @@ class MessageRestController {
     private String password;
  
 
+	@RequestMapping(value = "/whoami/{username}", method = RequestMethod.GET, produces = MediaType.TEXT_PLAIN_VALUE)
     public String whoami(@PathVariable("username") String username) {
         return String.format("Hello! You're %s your password is '%s'!\n", username, password);
     }
