9028431a26c12facee6d878bdcd2ac67a11c0808
Emina Huskic <huskic.emina@gmail.com>
2017-05-16 22:51:57 +0200

    registr

--- PMTool-AngularJS/app/components/sessions/registration.controller.js
+++ PMTool-AngularJS/app/components/sessions/registration.controller.js
@@ -1,7 +1,37 @@
-(function() {
+(function () {
     angular
         .module("NWT")
-        .controller("RegistrationController", ['$scope', 'DataFactory', function($scope, DataFactory) {
-            $scope.message = "Helloooo";
+        .controller("RegistrationController", ['$scope', '$http', 'SessionService', 'ToasterService', '$rootScope', '$location', function ($scope, $http, SessionService, ToasterService, $rootScope, $location) {
+
+            $scope.user = {
+                email: 'huskic@gmail.com',
+                username: 'admin',
+                password: 'admin',
+                name: 'emina',
+                bio: 'blablabla',
+                image: 'http://slika.jpg'
+            };
+            $scope.register = function () {
+                var dataObj = {
+                    email: $scope.user.email,
+                    username: $scope.user.username,
+                    password: $scope.user.password,
+                    name: $scope.user.name,
+                    bio: $scope.user.bio,
+                    image: $scope.user.image
+                };
+                $http({
+                    url:  NWTConfig.source + "register",
+                    method: "POST",
+                    data: { 'model': dataObj }
+                })
+                    .then(function (response) {
+                         redirectTo = (redirectTo == "/login") ? NWTConfig.DefaultRoute : redirectTo;
+                    $location.path(redirectTo);
+                    },
+                    function (response) { 
+                        alert( "failure message: " + JSON.stringify({data: data}));
+                    });
+            }
         }])
 }());
