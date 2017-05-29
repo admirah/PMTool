840a77af63c1eb7dd233fe9dcab73b88c596ac9f
Vejsil H <vhrustic1@etf.unsa.ba>
2017-05-29 20:48:48 +0200

    changed FinishedTask class

---
+++ ReportsModule/src/main/java/reports/models/FinishedTask.java
@@ -0,0 +1,42 @@
+package reports.models;
+
+import java.util.Date;
+
+public class FinishedTask {
+    private Long userId;
+    private String name;
+    private String taskName;
+    private Date finishedOn;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+    public String getTaskName() {
+        return taskName;
+    }
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+    public Date getFinishedOn() {
+        return finishedOn;
+    }
+    public void setFinishedOn(Date finishedOn) {
+        this.finishedOn = finishedOn;
+    }
+    public FinishedTask(Long userId, String name, String taskName, Date date) {
+        super();
+        this.userId = userId;
+        this.name = name;
+        this.taskName = taskName;
+        this.finishedOn = date;
+    }
+}
